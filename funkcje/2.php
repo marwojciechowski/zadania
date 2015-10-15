97e2c8b024e2d6131bc1a9b79850cf618898b9ee
marwojciechowski <mar.wojciechowski@interia.pl>
2015-10-15 17:00:34 +0200

    done job

---
+++ funkcje/2.php
@@ -0,0 +1,14 @@
+<?php
+
+$cars = array('Opel' => 'Insignia'
+				    ,'Toyota' => 'Avensis'
+					,'Renault'=>'Megane'
+					,'BMW'=>'E39'
+					,'Mercedes'=>'W123');
+
+function last_element($cars)
+{
+	return end($cars);
+}	
+					
+?>
